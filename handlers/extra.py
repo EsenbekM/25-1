bf615ce1846db259e4e55164dfdfc44358f5c31a
EsenbekM <esenbekm03@gmail.com>
2023-01-30 19:38:54 +0600

    added openai

--- handlers/extra.py
+++ handlers/extra.py
@@ -1,23 +1,25 @@
 from aiogram import types, Dispatcher
 from config import bot
+from test import get_message
 
 
 async def echo(message: types.Message):
-    bad_words = ['java', 'html', 'дурак', "дура"]
-    username = f"@{message.from_user.username}" \
-        if message.from_user.username is not None else message.from_user.first_name
-    for word in bad_words:
-        if word in message.text.lower().replace(' ', ''):
-            # DRY - Don't Repeat Yourself
-            await message.answer(f"Не матерись {username}"
-                                 f" сам ты {word}!")
-
-    if message.text.startswith('.'):
-        await bot.pin_chat_message(message.chat.id, message.message_id)
-
-    if message.text == 'dice':
-        a = await bot.send_dice(message.chat.id, emoji='🎯')
-        # print(a.dice.value)
+    await message.answer(get_message(message))
+    # bad_words = ['java', 'html', 'дурак', "дура"]
+    # username = f"@{message.from_user.username}" \
+    #     if message.from_user.username is not None else message.from_user.first_name
+    # for word in bad_words:
+    #     if word in message.text.lower().replace(' ', ''):
+    #         # DRY - Don't Repeat Yourself
+    #         await message.answer(f"Не матерись {username}"
+    #                              f" сам ты {word}!")
+    #
+    # if message.text.startswith('.'):
+    #     await bot.pin_chat_message(message.chat.id, message.message_id)
+    #
+    # if message.text == 'dice':
+    #     a = await bot.send_dice(message.chat.id, emoji='🎯')
+    #     # print(a.dice.value)
 
 
 def register_handlers_extra(dp: Dispatcher):
--- test.py
+++ test.py
@@ -1,48 +1,19 @@
-import time
-import asyncio
+from decouple import config
+import openai
+
+openai.api_key = config("OPENAI_API_KEY")
 
 
-# async def download_photo(photo_count, limit):
-#     while photo_count < limit:
-#         await asyncio.sleep(5)
-#         photo_count += 1
-#         print(f"Photo {photo_count}...")
-#
-#
-# async def download_video(video_count, limit):
-#     while video_count < limit:
-#         await asyncio.sleep(1)
-#         video_count += 1
-#         print(f"video {video_count}...")
-#
-#
-# async def main():
-#     photo_count = 0
-#     video_count = 0
-#
-#     task_list = [
-#         download_photo(photo_count, 10),
-#         download_video(video_count, 5)
-#     ]
-#     await asyncio.gather(*task_list)
-#
-# asyncio.run(main())
+def get_message(message):
+    response = openai.Completion.create(
+      model="text-davinci-003",
+      prompt=message.text,
+      temperature=0.5,
+      max_tokens=1000,
+      top_p=1.0,
+      frequency_penalty=0.5,
+      presence_penalty=0.0,
+    )
 
-async def download_photo(photo_count):
-    await asyncio.sleep(1)
-    print(f"Photo {photo_count}...")
+    return response['choices'][0]['text']
 
-
-async def main():
-    limit = int(input("Enter amount: "))
-    current_photo = 0
-    task_list = []
-
-    while current_photo < limit:
-        current_photo += 1
-        task = asyncio.create_task(download_photo(current_photo))
-        task_list.append(task)
-
-    await asyncio.gather(*task_list)
-
-asyncio.run(main())
